f78de868caf8b858156ea55cc8c908f0fc59ab00
maviz991 <matheus.aviz27@gmail.com>
2026-04-30 14:00:41 -0300

    versao 2 - cj municipal. Falta sub capital

---
+++ output/tjsp_municipios_style.sld
@@ -0,0 +1,232 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.1.0/StyledLayerDescriptor.xsd" xmlns:se="http://www.opengis.net/se">
+  <NamedLayer>
+    <se:Name>tjsp_municipios_sp</se:Name>
+    <UserStyle>
+      <se:Name>tjsp_municipios_sp</se:Name>
+      <se:FeatureTypeStyle>
+        
+        <se:Rule>
+          <se:Name>10ª RAJ Sorocaba</se:Name>
+          <se:Description>
+            <se:Title>10ª RAJ Sorocaba</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>10ª RAJ Sorocaba</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#e6194b</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>1ª RAJ Grande São Paulo</se:Name>
+          <se:Description>
+            <se:Title>1ª RAJ Grande São Paulo</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>1ª RAJ Grande São Paulo</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#3cb44b</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>2ª RAJ Araçatuba</se:Name>
+          <se:Description>
+            <se:Title>2ª RAJ Araçatuba</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>2ª RAJ Araçatuba</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#ffe119</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>3ª RAJ Bauru</se:Name>
+          <se:Description>
+            <se:Title>3ª RAJ Bauru</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>3ª RAJ Bauru</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#4363d8</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>4ª RAJ Campinas</se:Name>
+          <se:Description>
+            <se:Title>4ª RAJ Campinas</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>4ª RAJ Campinas</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#f58231</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>5ª RAJ Presidente Prudente</se:Name>
+          <se:Description>
+            <se:Title>5ª RAJ Presidente Prudente</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>5ª RAJ Presidente Prudente</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#911eb4</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>6ª RAJ Ribeirão Preto</se:Name>
+          <se:Description>
+            <se:Title>6ª RAJ Ribeirão Preto</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>6ª RAJ Ribeirão Preto</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#46f0f0</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>7ª RAJ Santos</se:Name>
+          <se:Description>
+            <se:Title>7ª RAJ Santos</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>7ª RAJ Santos</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#f032e6</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>8ª RAJ São José do Rio Preto</se:Name>
+          <se:Description>
+            <se:Title>8ª RAJ São José do Rio Preto</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>8ª RAJ São José do Rio Preto</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#bcf60c</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>9ª RAJ São José dos Campos</se:Name>
+          <se:Description>
+            <se:Title>9ª RAJ São José dos Campos</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>9ª RAJ São José dos Campos</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">#fabebe</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>
+      </se:FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>
--- process_juridico_data.py
+++ process_juridico_data.py
@@ -4,6 +4,9 @@ import geopandas as gpd
 from shapely.geometry import Point
 import os
 import json
+import io
+import time
+import re
 
 # Configuration
 FOROS_INTERESSE_PATH = "dados_input/foros_interesse_principal.csv"
@@ -13,186 +16,164 @@ MAPPING_CSV = REPO_BASE_URL + "Unidades%2C%20Munic%C3%ADpios%20e%20Comarcas.csv"
 UNIDADES_CSV = REPO_BASE_URL + "Unidades.csv"
 
 IBGE_GEO_URL = "https://servicodados.ibge.gov.br/api/v3/malhas/estados/35?formato=application/vnd.geo+json&qualidade=maxima&intrarregiao=municipio"
-SIDRA_URL = "https://api.sidra.ibge.gov.br/v1/values/t/9514/v/93/p/2022/n6/all?f=c"
-
 OUTPUT_DIR = "output"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
-
-import io
-import time
 
 def fetch_with_retry(url, method="GET", max_retries=3, delay=2, **kwargs):
     for i in range(max_retries):
         try:
-            if method == "GET":
-                r = requests.get(url, timeout=60, verify=False, **kwargs)
-            else:
-                r = requests.post(url, timeout=60, verify=False, **kwargs)
+            r = requests.get(url, timeout=60, verify=False, **kwargs) if method == "GET" else requests.post(url, timeout=60, verify=False, **kwargs)
             r.raise_for_status()
             return r
         except Exception as e:
-            if i == max_retries - 1:
-                raise e
+            if i == max_retries - 1: raise e
             print(f"Retry {i+1}/{max_retries} for {url} due to {e}")
             time.sleep(delay)
 
+def generate_sld_poly(rajs):
+    # Palette of 10 pleasant colors for RAJs
+    colors = ["#e6194b", "#3cb44b", "#ffe119", "#4363d8", "#f58231", "#911eb4", "#46f0f0", "#f032e6", "#bcf60c", "#fabebe"]
+    rules = ""
+    for i, raj in enumerate(sorted(rajs)):
+        color = colors[i % len(colors)]
+        rules += f"""
+        <se:Rule>
+          <se:Name>{raj}</se:Name>
+          <se:Description>
+            <se:Title>{raj}</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsEqualTo>
+              <ogc:PropertyName>nome_raj</ogc:PropertyName>
+              <ogc:Literal>{raj}</ogc:Literal>
+            </ogc:PropertyIsEqualTo>
+          </ogc:Filter>
+          <se:PolygonSymbolizer>
+            <se:Fill>
+              <se:SvgParameter name="fill">{color}</se:SvgParameter>
+              <se:SvgParameter name="fill-opacity">0.6</se:SvgParameter>
+            </se:Fill>
+            <se:Stroke>
+              <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+              <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+            </se:Stroke>
+          </se:PolygonSymbolizer>
+        </se:Rule>"""
+    
+    sld = f"""<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.1.0/StyledLayerDescriptor.xsd" xmlns:se="http://www.opengis.net/se">
+  <NamedLayer>
+    <se:Name>tjsp_municipios_sp</se:Name>
+    <UserStyle>
+      <se:Name>tjsp_municipios_sp</se:Name>
+      <se:FeatureTypeStyle>
+        {rules}
+      </se:FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>"""
+    return sld
+
 def main():
     print("Step 1: Loading inputs and GitHub data (June 2024 update)...")
-    # Load foros of interest
-    df_interesse = pd.read_csv(FOROS_INTERESSE_PATH, sep=';')
-    df_interesse['foro_clean'] = df_interesse['foros_interesse_principal'].str.replace("Foro de ", "").str.strip()
-    foros_list = set(df_interesse['foro_clean'].tolist())
-    
-    # Load mappings from GitHub
     r_mapping = fetch_with_retry(MAPPING_CSV)
     df_mapping = pd.read_csv(io.StringIO(r_mapping.text))
     r_unidades = fetch_with_retry(UNIDADES_CSV)
     df_unidades = pd.read_csv(io.StringIO(r_unidades.text))
     
-    # Clean up mapping
     df_mapping = df_mapping.map(lambda x: x.strip() if isinstance(x, str) else x)
-    
-    # Propagate RAJ info: Some municipalities have "0ª RAJ" but others in the same Comarca have the correct one
-    # 1. Identify "bad" RAJs
     df_mapping['is_bad_raj'] = df_mapping['raj'].str.contains("0ª RAJ", na=True)
-    
-    # 2. For each Comarca, find the best RAJ (first non-bad one)
     comarca_rajs = df_mapping.sort_values(by=['comarca_tjsp', 'is_bad_raj']).drop_duplicates(subset='comarca_tjsp')[['comarca_tjsp', 'raj']]
     comarca_rajs.columns = ['comarca_tjsp', 'best_raj']
-    
-    # 3. Merge back and override
     df_mapping = df_mapping.merge(comarca_rajs, on='comarca_tjsp', how='left')
     df_mapping['raj'] = df_mapping['best_raj']
-    
-    # 4. Final deduplication by municipality
     df_mapping = df_mapping.drop_duplicates(subset='id_municipio', keep='first')
     
     POP_API_URL = "https://servicodados.ibge.gov.br/api/v3/agregados/9514/periodos/2022/variaveis/93?localidades=N6[all]"
     print("Step 2: Fetching IBGE Population (2022 Census)...")
     pop_dict = {}
-    try:
-        r_pop = fetch_with_retry(POP_API_URL, max_retries=2)
-        pop_json = r_pop.json()
-        series = pop_json[0]['resultados'][0]['series']
-        for s in series:
-            code = int(s['localidade']['id'])
-            if str(code).startswith('35'): # Only SP
-                val = s['serie']['2022']
-                pop_dict[code] = int(val) if val and val != "..." else 0
-        print(f"  - Successfully fetched population for {len(pop_dict)} municipalities.")
-    except Exception as e:
-        print(f"WARNING: Could not fetch population data ({e}). Continuing with 0 values.")
+    r_pop = fetch_with_retry(POP_API_URL)
+    series = r_pop.json()[0]['resultados'][0]['series']
+    for s in series:
+        code = int(s['localidade']['id'])
+        if str(code).startswith('35'):
+            val = s['serie']['2022']
+            pop_dict[code] = int(val) if val and val != "..." else 0
     
     print("Step 3: Fetching IBGE Geometries...")
     r_geo = fetch_with_retry(IBGE_GEO_URL)
-    geo_json = r_geo.json()
-    
-    if 'features' in geo_json:
-        gdf_mun = gpd.GeoDataFrame.from_features(geo_json['features'])
-    elif isinstance(geo_json, list):
-        gdf_mun = gpd.GeoDataFrame.from_features(geo_json)
-    else:
-        # Check if it's a TopoJSON or other
-        print(f"Unexpected GeoJSON structure. Keys: {geo_json.keys() if isinstance(geo_json, dict) else 'Not a dict'}")
-        # Try to use it directly if it's a FeatureCollection without 'features' key (unlikely)
-        gdf_mun = gpd.GeoDataFrame.from_features(geo_json)
+    gdf_mun = gpd.GeoDataFrame.from_features(r_geo.json()['features'])
     gdf_mun['cod_mun'] = gdf_mun['codarea'].astype(int)
     gdf_mun['populacao_2022'] = gdf_mun['cod_mun'].map(pop_dict).fillna(0).astype(int)
     
-    print("Step 4: Merging and Processing (Entire State with Rich Metadata)...")
-    
-    # Get CJ info for each Comarca from df_unidades
-    df_comarca_cj = df_unidades[['comarca_corrigido', 'cj', 'id_cj']].drop_duplicates(subset='comarca_corrigido')
-    
-    # Merge mapping with CJ info
-    df_full_map = df_mapping.merge(df_comarca_cj, left_on='comarca_tjsp', right_on='comarca_corrigido', how='left')
-    
-    # Ensure one entry per municipality to avoid duplicate geometries
-    df_mapping_unique = df_full_map.drop_duplicates(subset='id_municipio').copy()
-    
-    # Pre-calculate aggregated lists of cities
-    foro_cities = df_mapping_unique.groupby('comarca_tjsp', sort=False)['municipio_tjsp'].apply(lambda x: ", ".join(sorted(x.astype(str)))).reset_index()
+    print("Step 4: Processing Judicial Mapping...")
+    foro_cities = df_mapping.groupby('comarca_tjsp')['municipio_tjsp'].apply(lambda x: ", ".join(sorted(x.astype(str)))).reset_index()
     foro_cities.columns = ['comarca_tjsp', 'Lista Cidades Foro']
     
-    # Note: 'cj' comes from df_unidades
-    cj_cities = df_mapping_unique.groupby('cj', sort=False)['municipio_tjsp'].apply(lambda x: ", ".join(sorted(x.astype(str)))).reset_index()
-    cj_cities.columns = ['cj', 'Lista Cidades CJ']
+    df_unidades_cj = df_unidades[['comarca_corrigido', 'cj', 'id_cj']].drop_duplicates(subset='comarca_corrigido')
+    df_mapping_rich = df_mapping.merge(foro_cities, on='comarca_tjsp', how='left').merge(df_unidades_cj, left_on='comarca_tjsp', right_on='comarca_corrigido', how='left')
     
-    # Merge mappings with these lists
-    df_mapping_rich = df_mapping_unique.merge(foro_cities, on='comarca_tjsp', how='left')
+    cj_cities = df_mapping_rich.groupby('cj')['municipio_tjsp'].apply(lambda x: ", ".join(sorted(x.astype(str)))).reset_index()
+    cj_cities.columns = ['cj', 'Lista Cidades CJ']
     df_mapping_rich = df_mapping_rich.merge(cj_cities, on='cj', how='left')
     
-    # Merge IBGE Geometries with Rich Mapping
     gdf_filtered = gdf_mun.merge(df_mapping_rich, left_on='cod_mun', right_on='id_municipio', how='inner')
+    gdf_filtered['id_foro'] = pd.factorize(gdf_filtered['comarca_tjsp'])[0] + 1
+    def extract_raj_id(raj_str):
+        if pd.isna(raj_str): return 0
+        match = re.search(r'(\d+)', str(raj_str))
+        return int(match.group(1)) if match else 0
+    gdf_filtered['id_raj_num'] = gdf_filtered['raj'].apply(extract_raj_id)
     
-    # Prepare metadata for Polygons
-    meta_cols = {
-        'comarca_tjsp': 'Nome do Foro',
-        'id_comarca': 'ID Foro',
-        'cj': 'Nome da CJ',
-        'id_cj': 'ID CJ',
-        'raj': 'Nome da RAJ',
-        'municipio_tjsp': 'Nome do Municipio',
-        'populacao_2022': 'Populacao',
-        'Lista Cidades Foro': 'Cidades no Foro',
-        'Lista Cidades CJ': 'Cidades na CJ'
-    }
+    foro_pop = gdf_filtered.groupby('comarca_tjsp')['populacao_2022'].sum().reset_index()
+    foro_pop.columns = ['comarca_tjsp', 'pop_foro']
+    gdf_filtered = gdf_filtered.merge(foro_pop, on='comarca_tjsp', how='left')
     
-    available_cols = ['geometry'] + [c for c in meta_cols.keys() if c in gdf_filtered.columns]
-    gdf_poly = gdf_filtered[available_cols].copy()
-    gdf_poly.columns = ['geometry'] + [meta_cols[c] for c in available_cols if c != 'geometry']
-    
-    print("Step 5: Generating Layers...")
-    
-    # Layer 2: Municipalities (Polygons)
-    poly_path = os.path.join(OUTPUT_DIR, "municipios_atendidos_sp.geojson")
-    gdf_poly.to_file(poly_path, driver='GeoJSON', encoding='utf-8')
-    print(f"  - Rich Polygon layer saved to {poly_path}")
-    
-    # Layer 1: Forum Seats (Points)
+    print("Step 5: Synthesizing Master Data...")
     df_unidades['is_principal'] = df_unidades['unidade'].str.contains("PRINCIPAL", case=False, na=False)
-    df_seats_meta = df_unidades.sort_values(by=['comarca_corrigido', 'is_principal'], ascending=[True, False])
-    df_seats_meta = df_seats_meta.drop_duplicates(subset='comarca_corrigido')
+    df_seats_meta = df_unidades.sort_values(by=['comarca_corrigido', 'is_principal'], ascending=[True, False]).drop_duplicates(subset='comarca_corrigido')
     
-    df_seats_final = df_seats_meta.merge(df_mapping_rich.drop_duplicates(subset='comarca_tjsp'), 
-                                         left_on='comarca_corrigido', right_on='comarca_tjsp', how='inner')
-    
-    seats_spatial = []
+    seat_lookup_data = []
     name_col = next((c for c in gdf_mun.columns if c.lower() in ['nome', 'nm_mun', 'nm_municipio', 'name']), None)
-
-    for _, row in df_seats_final.iterrows():
-        foro = row['comarca_tjsp']
-        city = row['endereco_municipio']
-        address = f"{row['endereco_lougradouro']}, {row['endereco_cep']}"
-        
+    for _, row in df_seats_meta.iterrows():
+        foro, city, address = row['comarca_corrigido'], row['endereco_municipio'], f"{row['endereco_lougradouro']}, {row['endereco_cep']}"
         city_geo = gdf_mun[gdf_mun[name_col].str.upper() == city.upper()] if name_col else pd.DataFrame()
-        if city_geo.empty:
-            # Filter using the original column name in gdf_filtered
-            city_geo = gdf_filtered[gdf_filtered['comarca_tjsp'] == foro].head(1)
-            
+        if city_geo.empty: city_geo = gdf_filtered[gdf_filtered['comarca_tjsp'] == foro].head(1)
         if not city_geo.empty:
             geom = city_geo.iloc[0]['geometry'].centroid
-            seats_spatial.append({
-                'geometry': geom,
-                'ID Foro': row.get('id_comarca_x', row.get('id_comarca', '')),
-                'Nome do Foro': foro,
-                'Endereco': address,
-                'Nome da CJ': row.get('cj_x', row.get('cj', '')),
-                'ID CJ': row.get('id_cj_x', row.get('id_cj', '')),
-                'Nome da RAJ': row.get('raj', ''),
-                'Cidades no Foro': row.get('Lista Cidades Foro', ''),
-                'Cidades na CJ': row.get('Lista Cidades CJ', ''),
-                'Populacao Municipio Sede': city_geo.iloc[0]['populacao_2022'] if 'populacao_2022' in city_geo.columns else 0
-            })
-            
-    gdf_seats = gpd.GeoDataFrame(seats_spatial, crs=gdf_filtered.crs)
-    points_path = os.path.join(OUTPUT_DIR, "foros_sedes_sp.geojson")
-    gdf_seats.to_file(points_path, driver='GeoJSON', encoding='utf-8')
-    print(f"  - Rich Point layer saved to {points_path}")
+            seat_lookup_data.append({'comarca_tjsp': foro, 'endereco_sede': address, 'latitude': geom.y, 'longitude': geom.x})
+    df_seat_lookup = pd.DataFrame(seat_lookup_data)
     
-    print("\nProcessing complete!")
-    print(f"Total forums processed: {len(gdf_seats)}")
-    print(f"Total municipalities included: {len(gdf_poly)}")
+    df_master = gdf_filtered.merge(df_seat_lookup, on='comarca_tjsp', how='left')
+    meta_cols_map = {
+        'cod_mun': 'id_municipio_ibge', 'municipio_tjsp': 'nome_municipio', 'populacao_2022': 'populacao_municipio',
+        'id_foro': 'id_foro', 'comarca_tjsp': 'nome_foro', 'endereco_sede': 'endereco_sede',
+        'latitude': 'latitude', 'longitude': 'longitude', 'id_cj': 'id_cj', 'cj': 'nome_cj',
+        'id_raj_num': 'id_raj', 'raj': 'nome_raj', 'pop_foro': 'populacao_foro',
+        'Lista Cidades Foro': 'cidades_no_foro', 'Lista Cidades CJ': 'cidades_na_cj'
+    }
+    df_master = df_master.rename(columns=meta_cols_map)
+    final_cols = ['id_municipio_ibge', 'nome_municipio', 'populacao_municipio', 'id_foro', 'nome_foro', 'endereco_sede', 'latitude', 'longitude', 'id_cj', 'nome_cj', 'id_raj', 'nome_raj', 'populacao_foro', 'cidades_no_foro', 'cidades_na_cj']
+    df_master = df_master[final_cols + ['geometry']]
+    
+    print("Step 6: Generating Final Outputs...")
+    df_master.drop(columns=['geometry']).to_csv(os.path.join(OUTPUT_DIR, "tjsp_mapeamento_bi.csv"), index=False, sep=';', encoding='utf-8-sig')
+    
+    gdf_poly = gpd.GeoDataFrame(df_master, geometry='geometry', crs=gdf_mun.crs)
+    gdf_poly.to_file(os.path.join(OUTPUT_DIR, "tjsp_municipios_sp.geojson"), driver='GeoJSON', encoding='utf-8')
+    
+    df_seats = df_master.drop_duplicates(subset=['nome_foro'])
+    gdf_seats = gpd.GeoDataFrame(df_seats, geometry=[Point(x, y) for x, y in zip(df_seats.longitude, df_seats.latitude)], crs=gdf_mun.crs)
+    gdf_seats.to_file(os.path.join(OUTPUT_DIR, "tjsp_foros_sedes_sp.geojson"), driver='GeoJSON', encoding='utf-8')
+    
+    gdf_raj = gdf_poly.dissolve(by='nome_raj', aggfunc='first').reset_index()
+    gdf_raj.to_file(os.path.join(OUTPUT_DIR, "tjsp_fronteiras_raj.geojson"), driver='GeoJSON', encoding='utf-8')
+    
+    # Generate SLD
+    with open(os.path.join(OUTPUT_DIR, "tjsp_municipios_style.sld"), "w", encoding="utf-8") as f:
+        f.write(generate_sld_poly(gdf_poly['nome_raj'].unique()))
+    
+    print("\nProcessing complete! SLD style generated in output/tjsp_municipios_style.sld")
 
 if __name__ == "__main__":
     main()
